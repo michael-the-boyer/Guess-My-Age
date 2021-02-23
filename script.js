8d04e9e9f65bc7665bc2e4f3c64d3e16e5011404
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-23 17:15:48 +0000

    Javascript no message lol

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function addressGuess () {
   const randomAge = Math.floor(Math.random() * 98)
   const answer = document.getElementById('answer')
   answer.innerHTML = 'The game has started! Keep plugging in numbers in the given prompt and see how many guesses it takes!'
-  let guess = prompt('Guess my age here!', randomAge)
+  let guess = prompt('Guess my age here!', '')
   guess = parseInt(guess)
   while (guess !== randomAge) {
     if (guess < randomAge) {
-      guess = prompt('Hmm, your guess is too low, try guessing higher.', randomAge)
+      guess = prompt('Hmm, your guess is too low, try guessing higher.', '')
       guess = parseInt(guess)
     } else if (guess > randomAge) {
-      guess = prompt('Woah, your guess is too high! Lower! Lower!', randomAge)
+      guess = prompt('Woah, your guess is too high! Lower! Lower!', '')
       guess = parseInt(guess)
     } else {
-      guess = prompt('Something went wrong, make sure you are plugging in a numerical value.', randomAge)
+      guess = prompt('Something went wrong, make sure you are plugging in a numerical value.', '')
     }
   }
   answer.innerHTML = 'Wow, you got it! ' + randomAge + ' was my age! Click the button to play again!'
